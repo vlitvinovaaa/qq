bf35d8008115569fe648770004f24552ee177c1d
vlit <vlit@ciklum.com>
2019-06-21 18:27:18 +0300

    untitled commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,42 @@
 import { Component } from '@angular/core';
+export class Hero {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes"></li>
+    </ul>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+ public title = 'Tour of Heroes';
+ public hero: Hero = {
+   id: 1,
+   name: 'Windstorm'
+ };
+ public heroes = HEROES;
+}
+
+let HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
